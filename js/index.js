f5e30b8a3a6498bdee2ebd2542bc209f041d063e
lilyhoratio <lilyhzhou@gmail.com>
2019-06-18 13:37:23 -0700

    added event listener for on click

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+const logoHeading = document.getElementById("logo-heading");
+const busImg = document.querySelector("header img");
+const welcomeText = document.getElementById()
+
+busImg.addEventListener("click", function() {
+    document.getElementsByTagName("h2") + "!"
+})
