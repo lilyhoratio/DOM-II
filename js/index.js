f82b5174abf2c2ccbac955672213ab4b233b3cda
lilyhoratio <lilyhzhou@gmail.com>
2019-06-18 14:45:42 -0700

    fixed on click event listener

--- js/index.js
+++ js/index.js
@@ -1,9 +1,32 @@
 // Your code goes here
 
-const logoHeading = document.getElementById("logo-heading");
-const busImg = document.querySelector("header img");
-const welcomeText = document.getElementById()
+// VARIABLES
+// navbar
+let mainNav = document.getElementsByClassName("main-navigation"); 
+let navHeader = document.getElementById("logo-heading");
 
-busImg.addEventListener("click", function() {
-    document.getElementsByTagName("h2") + "!"
+// top section
+let busImg = document.querySelector("header img");
+let welcomeText = document.querySelector("header h2")
+
+// middle section (let's go!)
+let middleSection = document.getElementsByClassName("content-section");
+let middleImgs = document.getElementsByClassName("img-content")
+
+// destination section
+let destinationSection = document.getElementsByClassName("content-destination");
+
+// bottom section
+let funInSun = document.querySelector(".destination h4") 
+let signUpButtons = document.querySelectorAll(".btn")
+
+// Event Listeners
+// Click on fun bus image which turns site into horror bus
+
+
+// Add "!" to h2 on click
+let h2Arr = Array.from(document.getElementsByTagName("h2"));
+
+busImg.addEventListener("click", function(event) {
+    h2Arr.forEach(element => { element.textContent += "!"})
 })
