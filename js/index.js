17204fde4419ab834deb6d79d515562d0cd6f0cf
lilyhoratio <lilyhzhou@gmail.com>
2019-06-18 15:23:52 -0700

    event listener on drag event

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@
 // navbar
 let mainNav = document.getElementsByClassName("main-navigation"); 
 let navHeader = document.getElementById("logo-heading");
+let navLinks = document.querySelectorAll("nav a");
 
 // top section
 let busImg = document.querySelector("header img");
@@ -20,13 +21,28 @@ let destinationSection = document.getElementsByClassName("content-destination");
 let funInSun = document.querySelector(".destination h4") 
 let signUpButtons = document.querySelectorAll(".btn")
 
-// Event Listeners
-// Click on fun bus image which turns site into horror bus
 
+
+
+// Event Listeners
+// Mouseover on "FunBus" for cat bus
+
+let bodyBackground = document.getElementsByClassName("body");
+welcomeText.addEventListener('mouseover', function(event) {
+    bodyBackground[0].style.backgroundColor = "black";
+})
 
 // Add "!" to h2 on click
 let h2Arr = Array.from(document.getElementsByTagName("h2"));
 
 busImg.addEventListener("click", function(event) {
     h2Arr.forEach(element => { element.textContent += "!"})
+})
+
+// Nav Links 
+navLinks.forEach(anchor => {
+    anchor.addEventListener("drag", event => {
+        anchor.style.color = "pink";
+        // event.target.classList.toggle('turnPink');
+    })
 })
