b88664348c3baa9046af4b452f2b0748d088830b
lilyhoratio <lilyhzhou@gmail.com>
2019-06-19 00:13:17 -0700

    added keydown, keyup, and click event listeners

--- js/index.js
+++ js/index.js
@@ -1,82 +1,96 @@
-// Your code goes here
-
 // VARIABLES
 // navbar
-let mainNav = document.getElementsByClassName("main-navigation");
-let navHeader = document.getElementsByClassName("logo-heading");
-let navLinks = document.querySelectorAll("nav a");
+const mainNav = document.getElementsByClassName("main-navigation");
+const navHeader = document.getElementsByClassName("logo-heading");
+const navLinks = document.querySelectorAll("nav a");
 
 // top section
-let busImg = document.querySelector("header img");
-let welcomeText = document.querySelector("header h2")
+const busImg = document.querySelector("header img");
+const welcomeText = document.querySelector("header h2")
 
 // middle section (let's go!)
-let middleSection = document.getElementsByClassName("content-section");
-let middleImgs = document.querySelectorAll(".img-content img")
+const middleSection = document.getElementsByClassName("content-section");
+const middleImgs = document.querySelectorAll(".img-content img")
 
 // destination section
-let destinationSection = document.getElementsByClassName("content-destination");
+const destinationSection = document.getElementsByClassName("content-destination");
 
 // bottom section
-let funInSun = document.querySelector(".destination h4")
-let signUpButtons = document.querySelectorAll(".btn")
-
-
-
+const signUpButtons = document.querySelectorAll(".btn")
+const footer = document.querySelector("footer")
 
 // EVENT LISTENERS
 
-// Mouseover - change background with mouseover "FunBus"
-
+// 1 - Mouseover: change background with mouseover "FunBus"
 // let body = document.getElementsByClassName("body");
-let body = document.querySelector("body");
+const body = document.querySelector("body");
 navHeader[0].addEventListener('mouseover', function(event) {
     body.style.backgroundColor = "lightgrey";
 })
 
-// Click - Add "!" to h2
-let h2Arr = Array.from(document.getElementsByTagName("h2"));
+// 2 - Click: add "!" to all h2s
+const h2Arr = Array.from(document.getElementsByTagName("h2"));
 
 busImg.addEventListener("click", function (event) {
     h2Arr.forEach(element => {
-        element.textContent += "!"
+        element.textContent += " !"
     })
 })
 
-// Drag - Nav Links change color and size
+// 3 - Drag: nav links change color and size
 navLinks.forEach(anchor => {
     anchor.addEventListener("drag", event => {
         anchor.style.color = "pink";
         anchor.style.fontSize = '2rem';
-        // event.target.classList.toggle('turnPink');
     })
 })
 
-// Wheel - paragraphs turn uppercase
-let paras = document.querySelectorAll("p");
+// 4 - Wheel: paragraphs turn uppercase
+const paras = document.querySelectorAll("p");
 paras.forEach(para => {
     para.addEventListener("wheel", event => {
         para.textContent = para.textContent.toUpperCase();
+
+        // Tried to get each character to turn uppercase in animation-like way
         // let para1 = "";
         // for (let i=0; i <para.textContent.length ;i++){
         //     let paraText = para.textContent;
         //     para1 += paraText[i].toUpperCase();
-        //     console.log(para1);
         // }
         // return para1;
     })
 })
 
-// Resize - window alerts
+// 5 - Resize: window alerts
 window.addEventListener("resize", function (event) {
     this.alert("resizin");
 });
 
-// Double click - flip/hide image
+// 6 - Double click: flip/hide image
 middleImgs.forEach(img => {
-    img.addEventListener("click", event => event.target.style.display = "none")
+    img.addEventListener("dblclick", event => event.target.style.display = "none")
 })
 
-// Keydown
+// 7 - Keydown
+window.addEventListener("keydown", event => {
+    if (event.key.match(/^[A-Za-z]+$/)) {
+        console.log("YOU TYPED A CHARACTER!")
+    }
+})
 
+// 8 - Keyup
+window.addEventListener("keyup", event => {
+    signUpButtons.forEach(button => button.style.backgroundColor = "yellow")
+})
 
+// 9 - Click: add padding to navbar
+mainNav[0].addEventListener("click", event => {
+    event.currentTarget.style.paddingLeft = "100px"
+})
+
+// 10 - Mouseover: footer to change text
+footer.addEventListener("mouseover", event => {
+    event.currentTarget.style.fontFamily = "Papyrus"
+})
+
+// figure out how to add new class styling and toggle it
